f6e7770dd4722a6ee600756b33e7ede9c27950c2
Nadsun <nadjasundal@icloud.com>
2025-08-25 12:36:30 +0200

    Update oppgave-2.php

--- oppgave-2.php
+++ oppgave-2.php
@@ -1,1 +1,11 @@
-
+<?php    /* Eksempel 1 */
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet skriver ut en "god dag"-melding med personens navn 
+*/
+  $fornavn=$_POST ["fornavn"];
+  $etternavn=$_POST ["etternavn"];
+  $alder=$_POST ["alder"];
+	
+  		print ("God dag $fornavn $etternavn, du er faktisk §alder år <br />");  
+?>
