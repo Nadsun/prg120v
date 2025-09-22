88c018ed036ab3c77df895242e8fdfeb3f55b34b
Nadsun <nadjasundal@icloud.com>
2025-09-22 16:57:58 +0200

    id

--- ovingsoppgave-5/oppgave-4.php
+++ ovingsoppgave-5/oppgave-4.php
@@ -1,27 +1,28 @@
-<?php  /* vis-alle-studier */
+<?php  /* vis-alle-emner */
 /*
 /*  Programmet skriver ut alle registrerte studiumer og emner
 */
   include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
-  $sqlSetning="SELECT * FROM studium;";
+  $sqlSetning="SELECT * FROM emne;";
   
   $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
     /* SQL-setning sendt til database-serveren */
 	
   $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
-  print ("<h3>Registrerte studium</h3>");
+  print ("<h3>Registrerte emner</h3>");
   print ("<table border=1>");  
-  print ("<tr><th align=left>Studiumkode</th> <th align=left>Studiumnavn</th></tr>"); 
+  print ("<tr><th align=left>Emnekode</th> <th align=left>Emnenavn</th> <th align=left>Studiumnkode</th></tr>"); 
 
   for ($r=1;$r<=$antallRader;$r++)
     {
       $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
-      $studiumkode=$rad["studiumkode"];        /* ELLER $postnr=$rad[0]; */
-      $studiumnavn=$rad["studiumnavn"];    /* ELLER $poststed=$rad[1]; */
+      $emnekode=$rad["emnekode"];        /* ELLER $postnr=$rad[0]; */
+      $emnenavn=$rad["emnenavn"];    /* ELLER $poststed=$rad[1]; */
+      $studiumkode=$rad["studiumkode"]; 
 
-      print ("<tr> <td> $studiumkode </td> <td> $studiumnavn </td> </tr>");
+      print ("<tr> <td> $emnekode </td> <td> $emnenavn </td> <td> $studiumkode </td> </tr>");
     }
   print ("</table>"); 
 ?>
--- ovingsoppgave-5/oppgave-5.php
+++ ovingsoppgave-5/oppgave-5.php
@@ -2,22 +2,24 @@
 
 ?> 
 
-<h3>Registrer studium </h3>
+<h3>Registrer Emne </h3>
 
-<form method="post" action="" id="registrerStudiumSkjema" name="registrerStudiumSkjema">
-  Studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
-  Studiumnavn <input type="text" id="studiumnavn" name="studiumnavn" required /> <br/>
-  <input type="submit" value="Registrer studium" id="registrerStudiumKnapp" name="registrerStudiumKnapp" /> 
+<form method="post" action="" id="registrerEmneSkjema" name="registrerEmneSkjema">
+  Emnekode <input type="text" id="emnekode" name="emnekode" required /> <br/>
+  Emnenavn <input type="text" id="emnenavn" name="emnenavn" required /> <br/>
+ Studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
+  <input type="submit" value="Registrer emne" id="registrerEmneKnapp" name="registrerEmneKnapp" /> 
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
 
 <?php 
   if (isset($_POST ["registrerStudiumKnapp"]))
     {
+      $emnekode=$_POST ["emnekode"];
+      $emnenavn=$_POST ["emnenavn"];
       $studiumkode=$_POST ["studiumkode"];
-      $studiumnavn=$_POST ["studiumnavn"];
 
-      if (!$studiumkode || !$studiumnavn)
+      if (!$emnekode || !$emnenavn || !$studiumkode)
         {
           print ("B&aring;de studiumkode og studiumnavn m&aring; fylles ut");
         }
@@ -25,21 +27,21 @@
         {
           include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
-          $sqlSetning="SELECT * FROM studium WHERE studiumkode='$studiumkode';";
+          $sqlSetning="SELECT * FROM emne WHERE emnekode='$emnekode';";
           $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
           $antallRader=mysqli_num_rows($sqlResultat); 
 
           if ($antallRader!=0)  /* poststedet er registrert fra før */
             {
-              print ("Studiumkode er registrert fra f&oslashr");
+              print ("Emnekode er registrert fra f&oslashr");
             }
           else
             {
-              $sqlSetning="INSERT INTO studium VALUES('$studiumkode','$studiumnavn');";
+              $sqlSetning="INSERT INTO emne VALUES('$emnekode','$emnenavn','$studiumkode');";
               mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
                 /* SQL-setning sendt til database-serveren */
 
-              print ("F&oslash;lgende studium er n&aring; registrert: $studiumkode $studiumnavn"); 
+              print ("F&oslash;lgende studium er n&aring; registrert: $emnekode $emnenavn $studiumkode "); 
             }
         }
     }
