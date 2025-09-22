39852276238b9ad915c3a7a9cb2856fea2cc072c
Nadsun <nadjasundal@icloud.com>
2025-09-22 11:05:34 +0200

    ff

---
+++ ovingsoppgave-5/oppgave-1.php
@@ -0,0 +1,9 @@
+</table>
+$poststed = array(
+  array('postnr' => '3117','poststed' => 'Tønsberg'),
+  array('postnr' => '3124','poststed' => 'Tønsberg'),
+  array('postnr' => '3183','poststed' => 'Holmestrand'),
+  array('postnr' => '3229','poststed' => 'Sandefjord'),
+  array('postnr' => '6869','poststed' => 'Hafslo'),
+  array('postnr' => '6899','poststed' => 'Balestrand')
+);
